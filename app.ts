67a9df33871c2de3729cea61ef35b808ce4bcd51
DindoLeonard <dindo.leonard@gmail.com>
2022-04-01 04:10:21 +0800

    first test

--- app.ts
+++
@@ -1,7 +0,0 @@
-import express, { Express } from 'express';
-
-const app: Express = express();
-
-app.listen(3000, () => {
-  console.log(`app is running`);
-});
---
+++ index.ts
@@ -0,0 +1,5 @@
+import { app } from './src/app';
+
+app.listen(3000, () => {
+  console.log(`app is running`);
+});
