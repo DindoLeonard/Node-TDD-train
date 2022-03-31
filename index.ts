e20348baaebf37dad49311854bb3a557575024dd
DindoLeonard <dindo.leonard@gmail.com>
2022-04-01 05:40:05 +0800

    Setup sequelize

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
 import { app } from './src/app';
+import sequelize from './src/config/database';
+
+// initializing sequalize
+sequelize.sync();
 
 app.listen(3000, () => {
   console.log(`app is running`);
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express, { Express, Request, Response } from 'express';
+import User from './user/User';
 
 export const app: Express = express();
 
+app.use(express.json());
+
 app.post('/api/1.0/users', (req: Request, res: Response) => {
-  return res.send({ message: 'User created' });
+  User.create(req.body).then(() => {
+    return res.send({ message: 'User created' });
+  });
 });
