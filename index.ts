814be2ab02a19bf959d3c967db2d3e335f171a7e
DindoLeonard <dindo.leonard@gmail.com>
2022-04-04 10:06:26 +0800

    Add configuration and environment for development and test

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import sequelize from './src/config/database';
 // initializing sequalize
 sequelize.sync();
 
+console.log('env ' + process.env.NODE_ENV);
+
 app.listen(3000, () => {
   console.log(`app is running`);
 });
