441febd52e626996f226cd0b1cdaf954a2172d18
DindoLeonard <dindo.leonard@gmail.com>
2022-04-12 01:29:34 +0800

    Refactoring test, create smtp server for testing instead of mocking with jest

--- src/config/emailTransporter.ts
+++ src/config/emailTransporter.ts
@@ -1,6 +1,14 @@
 import nodemailer from 'nodemailer';
-import nodemailerStub from 'nodemailer-stub';
+// import nodemailerStub from 'nodemailer-stub';
 
-const transporter = nodemailer.createTransport(nodemailerStub.stubTransport);
+// const transporter = nodemailer.createTransport(nodemailerStub.stubTransport);
+
+const transporter = nodemailer.createTransport({
+  host: 'localhost',
+  port: 8587,
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 
 export default transporter;
